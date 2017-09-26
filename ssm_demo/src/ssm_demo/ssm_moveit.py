efbe45d8c6e0a6370fdaf94b0da3badf4df742da
Harsh Deshpande <nhg-hd@ipa.fraunhofer.de>
2017-09-26 09:57:07 +0200

    Added states to make a complete ITR smartpick demo. Includes mostly empty states

--- ssm_demo/src/ssm_demo/ssm_moveit.py
+++ ssm_demo/src/ssm_demo/ssm_moveit.py
@@ -12,6 +12,7 @@ from gazebo_msgs.srv import SpawnModel
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose
 from moveit_msgs.msg import ExecuteTrajectoryActionResult 
+from moveit_msgs.msg import MoveItErrorCodes
 import ast
 import copy
 import actionlib
@@ -48,14 +49,14 @@ def get_pose(str):
 
 class MoveP2P(ssm_state.ssmState):
     def __init__(self):
-        ssm_state.ssmState.__init__(self, outcomes=['success'], io_keys=['target'])
+        ssm_state.ssmState.__init__(self, outcomes=['success', 'fail'], io_keys=['target'])
 
     def execution(self, ud):
         global get_pose
-        print ud.target
+        #print ud.target
         pose_target = get_pose(ud.target)
         group.set_pose_target(pose_target)
-        #print "Move group %s to target pose " % (group.get_end_effector_link())
+        print "Move group %s to target pose " % (group.get_name())
         print pose_target.position.x, \
               pose_target.position.y, \
               pose_target.position.z, \
@@ -65,7 +66,68 @@ class MoveP2P(ssm_state.ssmState):
               pose_target.orientation.w
 
         plan = group.plan()
-        group.execute(plan, wait=True)
+        if len(plan.joint_trajectory.points) == 0:
+            print "Plan failed! Exiting state machine."
+            return "fail"
+        err = group.execute(plan, wait=True)
+        print "execute response : "
+        print err
+        return "success"
+
+class MoveNamed(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success', 'fail'], io_keys=['pose_name'])
+
+    def execution(self, ud):
+        group.set_named_target(ud.pose_name)
+        plan = group.plan()
+        if len(plan.joint_trajectory.points) == 0:
+            print "Plan failed! Exiting state machine."
+            return "fail"
+        err = group.execute(plan, wait=True)
+        return "success"
+
+class MoveLine(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success', 'fail'], io_keys=['target'])
+
+    def execution(self, ud):
+        print "Move line"
+        return "success"
+
+#Only a parent state which calculates the approach pick and pick points and passes them to moveP2P
+#Is the calculation a separate state? Better to call it PrePick or InitPick or something like that
+class PickMove(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success', 'fail'], io_keys=['targets'])
+
+    def execution(self, ud):
+        print "performing pick"
+        return "success"
+
+class CalculatePickTrajectory(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success'], io_keys=['approach', 'pick', 'retract'])
+
+    def execution(self, ud):
+        print "calculating pick trajectory"
+        return "success"
+
+#Actually this is just a MoveP2P with gripper control -- no need for a separate state
+class PlaceMove(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success', 'fail'], io_keys=['pose_name'])
+
+    def execution(self, ud):
+        print "performing place"
+        return "success"
+
+class GripperControl(ssm_state.ssmState):
+    def __init__(self):
+        ssm_state.ssmState.__init__(self, outcomes=['success'], io_keys=['action'])
+
+    def execution(self, ud):
+        print "Open / close gripper"
         return "success"
 
 class InitRobot(ssm_state.ssmState):
@@ -75,15 +137,7 @@ class InitRobot(ssm_state.ssmState):
     def execution(self, ud):
         global group
         group = moveit_commander.MoveGroupCommander(ud.group)
-        print group.get_end_effector_link()
+        #print group.get_end_effector_link()
         return "success"
 
-#class HomeRobot(ssm_state.ssmState):
-#    def __init__(self):
-#        ssm_state.ssmState.__init__(self,outcomes=['success'], io_keys=['target'])
-#
-#    def execution(self, ud):
-#        global eulerToQuaternion
-#        ud.target = [0.3, 0, 0.8, 0, 90, 0]
-#
-#        return "success"
+
